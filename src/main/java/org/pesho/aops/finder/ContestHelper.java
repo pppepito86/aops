5da181b2e509e43435c01fb6aef6e489c77af47d
Petar Petrov <pppepito86@gmail.com>
2024-07-28 01:16:44 +0300

    add contests

--- src/main/java/org/pesho/aops/finder/ContestHelper.java
+++ src/main/java/org/pesho/aops/finder/ContestHelper.java
@@ -12,7 +12,22 @@ public class ContestHelper {
     public static final String USA_TSTST = "3424";
     public static final String ROMANIAN_MASTERS = "3238";
     public static final String ELMO_SHORTLIST = "3430";
+    public static final String BALKAN = "3225";
+    public static final String BALKAN_SHORTLIST = "915845";
+
+    public static final String BULGARIA_NATIONAL = "3275";
+    public static final String BULGARIA_TST = "3276";
+    public static final String BULGARIA_AUTUMN = "3011607";
+    public static final String BULGARIA_WINTER = "3741308";
+    public static final String BULGARIA_SPRING = "3016185";
+    public static final String BULGARIA_EGMO_TST = "3379480";
+    public static final String BULGARIA_JBMO_TST = "854080";
+    public static final String BULGARIA_FESTIVAL = "909811";
+
     public static final String CHINA_NATIONAL = "3284";
+    public static final String CHINA_NORTHERN = "3286";
+    public static final String CHINA_WESTERN = "3283";
+    public static final String CHINA_SOUTH_EAST = "3285";
     public static final String CHINA_TST = "3282";
     public static final String CHINA_GIRLS = "3287";
     public static final String RUSSIA_NATIONAL = "3371";
@@ -42,7 +57,11 @@ public class ContestHelper {
     public static final String PUTNAM = "3249";
 
     public static final List<String> ALL = Arrays.asList(IMO_SHORTLIST, USAMO, USAJMO, USA_TSTST, USA_TST,
-            ROMANIAN_MASTERS, ELMO_SHORTLIST, CHINA_NATIONAL, CHINA_TST, CHINA_GIRLS,
+            ROMANIAN_MASTERS, ELMO_SHORTLIST,
+            BALKAN, BALKAN_SHORTLIST,
+            BULGARIA_NATIONAL, BULGARIA_TST, BULGARIA_AUTUMN, BULGARIA_WINTER, BULGARIA_SPRING, BULGARIA_EGMO_TST, BULGARIA_JBMO_TST, BULGARIA_FESTIVAL,
+            CHINA_NATIONAL, CHINA_TST, CHINA_GIRLS,
+            CHINA_NORTHERN, CHINA_WESTERN, CHINA_SOUTH_EAST,
             RUSSIA_NATIONAL, RUSSIA_TST, RUSSIA_SHARYGIN_GEOMETRY, KOREA_NATIONAL, KOREA_FINAL_ROUND,
             JAPAN_NATIONAL, JAPAN_TST, ROMANIA_NATIONAL, ROMANIA_TST,
             VIETNAM_NATIONAL, VIETNAM_TST, CANADA_NATIONAL, CANADA_TST,
